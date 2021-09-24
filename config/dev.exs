ef226426db6c7714c6ecbf40df86377c312acde0
Blake Kostner <git@btkostner.io>
2021-09-24 17:52:00 -0600

    setup rabbitmq instance for during tests

--- config/dev.exs
+++ config/dev.exs
@@ -11,3 +11,16 @@ config :assembly, Assembly.Repo,
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
+
+config :amqp,
+  connections: [
+    rabbitmq_conn: [
+      username: "assembly",
+      password: "system76",
+      host: "localhost",
+      port: 5672
+    ]
+  ],
+  channels: [
+    events: [connection: :rabbitmq_conn]
+  ]
--- config/test.exs
+++ config/test.exs
@@ -12,4 +12,18 @@ config :assembly, Assembly.Repo,
   hostname: Map.get(System.get_env(), "DB_HOST", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox
 
+config :amqp,
+  connections: [
+    rabbitmq_conn: [
+      username: "assembly",
+      password: "system76",
+      host: Map.get(System.get_env(), "RABBITMQ_HOST", "localhost"),
+      port: 5672
+    ]
+  ],
+  channels: [
+    events: [connection: :rabbitmq_conn]
+  ]
+
+
 config :grpc, start_server: false
